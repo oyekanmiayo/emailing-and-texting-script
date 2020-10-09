6660468f7afb037046900caa51170647be1984cd
Ayomide Oyekanmi <ayomideoyekanmi@gmail.com>
2020-10-09 22:22:53 +0100

    Modifications

--- src/main/java/com/communicator/emailingandtextingscript/EmailingAndTextingScriptApplication.java
+++ src/main/java/com/communicator/emailingandtextingscript/EmailingAndTextingScriptApplication.java
@@ -9,7 +9,6 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
