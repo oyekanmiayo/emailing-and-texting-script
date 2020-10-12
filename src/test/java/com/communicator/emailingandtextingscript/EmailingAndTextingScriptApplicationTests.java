700ecedc3956aadb8343a356c88c462916ac0eb8
fernando.almeida <almeidascvn@gmail.com>
2020-10-12 11:24:50 +0100

    #7 - Added simple testCommunicationObjectsReading test case

--- src/test/java/com/communicator/emailingandtextingscript/EmailingAndTextingScriptApplicationTests.java
+++ src/test/java/com/communicator/emailingandtextingscript/EmailingAndTextingScriptApplicationTests.java
@@ -1,13 +1,24 @@
 package com.communicator.emailingandtextingscript;
 
+import com.communicator.emailingandtextingscript.scripts.ScheduledMessagingScript;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class EmailingAndTextingScriptApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void testCommunicationObjectsReading(){
+        ScheduledMessagingScript messagingScript = new ScheduledMessagingScript(null, null);
+        File testFile = new File("src/test/resources/communication_test.json");
+        messagingScript.initCommunicationObjects(testFile);
+
+        assertEquals(150, messagingScript.getCommunications().size());
+
+    }
 
 }
